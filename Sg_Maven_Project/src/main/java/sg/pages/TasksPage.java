b5b945529ce8afdd8f9fdc040a19f6a0e623c64f
SgTesting <chandrabhaga1408@gmail.com>
2024-03-19 23:31:07 +0530

    added space in TasksPage class

--- Sg_Maven_Project/src/main/java/sg/pages/TasksPage.java
+++ Sg_Maven_Project/src/main/java/sg/pages/TasksPage.java
@@ -7,4 +7,5 @@ public class TasksPage {
     public static By obj_EnterCustomer_Edit=By.xpath("//input[@id='customerLightBox_nameField']");
     public static By obj_AddDescription_Edit=By.xpath("//input[@class='inputFieldWithPlaceholder']");
     public static By obj_CreateCustomer_Button=By.xpath("//div[@id='customerLightBox_commitBtn']");
+
 }
