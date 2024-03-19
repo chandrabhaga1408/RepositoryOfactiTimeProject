42d1cec959edb0e97c6df999e4908d98ba2abeec
SgTesting <chandrabhaga1408@gmail.com>
2024-03-19 20:22:50 +0530

    Initial framework draft

---
+++ Sg_Maven_Project/src/main/java/sg/pages/TasksPage.java
@@ -0,0 +1,10 @@
+package sg.pages;
+
+import org.openqa.selenium.By;
+
+public class TasksPage {
+    public static By obj_AddNew_Button=By.xpath("//div[@class='addNewContainer']//div[@class='addNewButton']");
+    public static By obj_EnterCustomer_Edit=By.xpath("//input[@id='customerLightBox_nameField']");
+    public static By obj_AddDescription_Edit=By.xpath("//input[@class='inputFieldWithPlaceholder']");
+    public static By obj_CreateCustomer_Button=By.xpath("//div[@id='customerLightBox_commitBtn']");
+}
